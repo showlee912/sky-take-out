286a304afb251d7ea66fb6e70dcd6fce3390f046
showlee912 <showlee912@outlook.com>
2024-10-27 15:45:00 +0800

    分类管理+员工信息编辑

--- sky-pojo/src/main/java/com/sky/dto/CategoryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/CategoryDTO.java
@@ -7,10 +7,10 @@ import java.io.Serializable;
 @Data
 public class CategoryDTO implements Serializable {
 
-    //主键
+    //主键id
     private Long id;
 
-    //类型 1 菜品分类 2 套餐分类
+    //类型： 1为菜品分类 2为套餐分类
     private Integer type;
 
     //分类名称
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -42,5 +42,14 @@ public interface EmployeeMapper {
      * @param employee 员工
      */
     void update(Employee employee);
+
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
 
